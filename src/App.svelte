8c658416072be142b9c668d2b15c3fcbada41aad
Max Rubel <maxrubel1@gmail.com>
2024-10-16 02:18:50 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+
+  let counter = 0;
+  let intervalId: number | null = null;
+  let direction = "right";
+  let posX = 1;
+  let posY = 0;
+  const INTERVAL_TIME = 35; // Speed of character
+  const CONTAINER_WIDTH = 66; // Adjust based on container size
+  const CONTAINER_HEIGHT = 45; // Adjust based on container size
+
+  // prettier-ignore
+  const gameBoard = [
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  function startCounter() {
+    intervalId = setInterval(() => {
+      counter++;
+      moveShape();
+    }, INTERVAL_TIME);
+  }
+
+  function moveShape() {
+    switch (direction) {
+      case "right":
+        posX++;
+        if (posX >= CONTAINER_WIDTH) {
+          posX = CONTAINER_WIDTH;
+          direction = posY === 0 ? "up" : "down";
+        }
+        break;
+      case "up":
+        posY++;
+        if (posY >= CONTAINER_HEIGHT) {
+          posY = CONTAINER_HEIGHT;
+          direction = posX === CONTAINER_WIDTH ? "left" : "right";
+        }
+        break;
+      case "left":
+        posX--;
+        if (posX <= 0) {
+          posX = 0;
+          direction = posY === CONTAINER_HEIGHT ? "down" : "up";
+        }
+        break;
+      case "down":
+        posY--;
+        if (posY <= 0) {
+          posY = 0;
+          direction = posX === 0 ? "right" : "left";
+        }
+        break;
+    }
+
+    function handleKeyDown(e: KeyboardEvent) {
+      switch (e.key) {
+        case "ArrowLeft":
+          direction = "left";
+          break;
+        case "ArrowRight":
+          direction = "right";
+          break;
+        case "ArrowDown":
+          direction = "down";
+          break;
+        case "ArrowUp":
+          direction = "up";
+          break;
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // onMount(() => {
+    //   document.addEventListener("keydown", handleKeyDown);
+    // });
+    // onDestroy(() => {
+    //   document.removeEventListener("keydown", handleKeyDown);
+    // });
+  }
+
+  function stopCounter() {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+  }
+
+  function clearCounter() {
+    stopCounter();
+    posX = 1;
+    posY = 0;
+    counter = 0;
+    direction = "right";
+  }
+
+  onDestroy(() => {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+  });
+</script>
+
+<div style="text-align: left">
+  <button on:click={startCounter}>Start Counter</button>
+  <button on:click={stopCounter}>Pause Counter</button>
+  <button on:click={clearCounter}>Clear Counter</button>
+</div>
+
+<div style="text-align: left; margin-top: 1em">
+  Direction: {direction}, Counter: {counter}
+</div>
+
+<div class="game-container">
+  <div class="game-piece" style="left: {posX * 10}px; bottom: {posY * 10}px">
+    😁
+  </div>
+</div>
+
+<style>
+  .game-container {
+    position: relative;
+    border: 1px solid grey;
+    border-radius: 20px;
+    height: 500px;
+    width: 700px;
+    padding: 1em;
+  }
+
+  .game-piece {
+    position: absolute;
+    font-size: 48px;
+  }
+</style>
